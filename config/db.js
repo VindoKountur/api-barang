0cf5db522a377b91d36d36e9666b965a700fb130
Vindo Kountur <vindokountur@gmail.com>
2020-04-28 05:10:25 +0800

    change from localhost

--- config/db.js
+++ config/db.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
-    // LOCALHOST
-    const conn = await mongoose.connect('mongodb://localhost/warong', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    // LOCALHOST
+    // const conn = await mongoose.connect('mongodb://localhost/warong', {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
     console.log(`MongoDB connected host : ${conn.connection.host}`);
   } catch (err) {
     console.log(`Error connecting : ${err.message}`);
